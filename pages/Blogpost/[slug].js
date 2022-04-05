4ce591a948904bf1ba439e781ee2eeafe076def9
CoolD69 <parthasingharoy166@gmail.com>
2022-04-05 17:06:37 +0530

    blospost ready slug

--- pages/Blogpost/[slug].js
+++ pages/Blogpost/[slug].js
@@ -1,11 +1,21 @@
 import { useRouter } from 'next/router'
+import styles from '../../styles/Blogpost.module.css'
 import React from 'react'
 
 const slug = () => {
     const router = useRouter();
     const {slug} = router.query;
   return (
-    <div>Slug : {slug}</div>
+    <div className={styles.container}>
+      <h2>Title : {slug}</h2>
+      <hr/>
+      <p>
+      JavaScript, often abbreviated JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries.
+      </p>
+
+
+
+    </div>
   )
 }
 
