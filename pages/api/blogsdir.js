04e6340c6088321dc765889c2efebeac52bf0cf2
CoolD69 <parthasingharoy166@gmail.com>
2022-04-06 21:08:14 +0530

    blog api working

--- pages/api/blogsdir.js
+++ pages/api/blogsdir.js
@@ -1,20 +1,31 @@
 import *as fs from 'fs'
 
-export default function handler(req, res) {
-
+export default async function handler(req, res) {
+let myfile;
+let allBlogs = [];
 //readfile function
-  fs.readdir(`blogdata`, (err, data) => {
+ let data = await fs.promises.readdir("blogdata")
 
-    //error checking
-    if (err) {
-      res.status(200).json({ error: "No such blogs Found !" })
-      console.log(err);
-    }
-    else {
-      res.status(200).json(data)
-    }
+ for (let index = 0; index < data.length; index++) {
+   const element = data[index];
+   myfile = await fs.promises.readFile(("blogdata/"+element),'utf-8');
+  //  console.log(myfile); //for debugging
+   allBlogs.push(JSON.parse(myfile))
+  }
+  res.status(200).json(allBlogs)
 
-  })
+//  (err, data) => {
+
+//     //error checking
+//     if (err) {
+//       res.status(200).json({ error: "No such blogs Found !" })
+//       console.log(err);
+//     }
+//     else {
+//       res.status(200).json(data)
+//     }
+
+//   })
   
 }
 
