83a7b4bb56268463442241aaa9a318271df71c46
CoolD69 <parthasingharoy166@gmail.com>
2022-04-07 16:29:18 +0530

    updated

--- pages/api/postcontact.js
+++ pages/api/postcontact.js
@@ -3,19 +3,19 @@ import *as fs from 'fs'
 export default function handler(req, res) {
 
     //genarated a random number
-    let num =Math.floor((Math.random() * 100) + 1);
+    let num = Math.floor((Math.random() * 100) + 1);
 
 
     if (req.method === 'POST') {
-      console.log(req.body);
-        fs.writeFile(`contactdata/${req.body.name+num}.json`, JSON.stringify(req.body), () => {
-            
-      res.status(200).json({message:"success"})
+        console.log(req.body);
+        fs.writeFile(`contactdata/${req.body.name + num}.json`, JSON.stringify(req.body), () => {
+
+            res.status(200).json({ message: "success" })
         })
     } else {
         // Handle any other HTTP method
-        
-      res.status(200).json(["Contact Us"])
+
+        res.status(200).json(["Contact Us"])
     }
 
 
