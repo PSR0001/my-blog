1ad21abaf6d9fdc9340115c513cb298079dad4d4
CoolD69 <parthasingharoy166@gmail.com>
2022-04-07 16:28:50 +0530

    contact api created

---
+++ pages/api/postcontact.js
@@ -0,0 +1,25 @@
+import *as fs from 'fs'
+
+export default function handler(req, res) {
+
+    //genarated a random number
+    let num =Math.floor((Math.random() * 100) + 1);
+
+
+    if (req.method === 'POST') {
+      console.log(req.body);
+        fs.writeFile(`contactdata/${req.body.name+num}.json`, JSON.stringify(req.body), () => {
+            
+      res.status(200).json({message:"success"})
+        })
+    } else {
+        // Handle any other HTTP method
+        
+      res.status(200).json(["Contact Us"])
+    }
+
+
+}
+
+
+
