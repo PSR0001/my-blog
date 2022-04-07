cd4de8b5c48fcc2c475636d839cece02c722ec7b
CoolD69 <parthasingharoy166@gmail.com>
2022-04-07 20:25:46 +0530

    contact us page form created

--- pages/Contactus.js
+++ pages/Contactus.js
@@ -1,8 +1,36 @@
 import React from 'react'
-
+import styles from '../styles/Contact.module.css'
 const Contactus = () => {
   return (
-    <div>This is Contactus</div>
+    <>
+    <div className={styles.container}>
+    <h1>Contact Us</h1>
+    <div className={styles.mainform}>
+      <form className={styles.form}>
+        <div className={styles.mb3}>
+          <label for="name" className={styles.formlabel}>Name : </label>
+          <input type="text" className={styles.formcontrol} id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className={styles.mb3}>
+          <label for="exampleInputEmail1" className={styles.formlabel}>Email address : </label>
+          <input type="email" className={styles.formcontrol} id="exampleInputEmail1" aria-describedby="emailHelp"/>
+        </div>
+        <div className={styles.mb3}>
+          <label for="exampleInputPassword1" className={styles.formlabel}>Phone Number :</label>
+          <input type="text" className={styles.formcontrol} id="exampleInputPassword1"/>
+        </div>
+        <div className={styles.mb3}>
+          <label for="exampleInputPassword1" className={styles.formlabel}>Description :</label>
+          <textarea className={styles.formcontrol2} id="exampleFormControlTextarea1" rows="5"></textarea>
+        </div>
+        
+        <button type="submit" className={styles.btn}>Submit</button>
+      </form>
+    </div>
+  </div>
+    
+    
+    </>
   )
 }
 
