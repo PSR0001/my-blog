19efacca6df6427f7c64c83c99ed6af2ccf53eb6
CoolD69 <parthasingharoy166@gmail.com>
2022-08-12 23:17:28 +0530

    add particle:

---
+++ components/Particle.js
@@ -0,0 +1,104 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import { useCallback } from "react";
+
+
+const Particle = () => {
+
+  const particlesInit = useCallback(async (engine) => {
+    //console.log(engine);
+    // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(engine);
+}, []);
+
+const particlesLoaded = useCallback(async (container) => {
+    await console.log(container);
+}, []);
+
+
+  return (
+    <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            className="w-1/3"
+            options={{
+                background: {
+                  
+                    color: {
+                        value: "",
+                    },
+                },
+                fpsLimit: 100,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: false,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: false,
+                            mode: "repulse",
+                        },
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 10,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#22c460",
+                    },
+                    links: {
+                        color: "#22c460",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    collisions: {
+                        enable: true,
+                    },
+                    move: {
+                        directions: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 1,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 60,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            }}
+        />
+    )
+}
+
+export default Particle
